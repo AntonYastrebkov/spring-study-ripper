07947a22d15aa25f660869812888fb6b65bf28a8
Anton Yastrebkov <anton.yastrebkoff@gmail.com>
2019-11-10 20:39:02 +0300

    New custom scope

--- src/main/java/com/epam/spring/screensaver/Config.java
+++ src/main/java/com/epam/spring/screensaver/Config.java
@@ -10,7 +10,7 @@ import java.util.Random;
 @ComponentScan(basePackages = "com.epam.spring.screensaver")
 public class Config {
     @Bean
-    @Scope("prototype")
+    @Scope("periodical")
     public Color color() {
         Random random = new Random();
         return new Color(
